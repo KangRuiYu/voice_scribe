bdd98f7b5fccff0382161cc3feab780f1095ca91
KangRuiYu <48875810+KangRuiYu@users.noreply.github.com>
2020-12-16 21:08:42 -0800

    Cleaned up code by splitting widgets into more widgets

--- lib/views/recorder_widget.dart
+++ lib/views/recorder_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:voice_scribe/models/recorder.dart';
+
 import 'package:voice_scribe/views/custom_buttons.dart';
 import 'package:voice_scribe/views/duration_display.dart';
 import 'package:voice_scribe/views/volume_display.dart';
@@ -12,39 +13,9 @@ class MiniRecorderDisplay extends StatelessWidget {
     return Consumer<Recorder>(
       builder: (context, recorder, child) {
         if (recorder.paused) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircularIconButton(
-                iconData: Icons.stop,
-                onPressed: recorder.stopRecording,
-              ),
-              CircularIconButton(
-                iconData: Icons.play_arrow,
-                onPressed: recorder.resumeRecording,
-              ),
-            ],
-          );
+          return _MiniPausedControls(recorder);
         } else if (recorder.recording) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              VolumeDisplay(recorder.progress, true),
-              const SizedBox(width: 8),
-              RoundedButton(
-                child: Row(
-                  children: [
-                    DurationDisplay(recorder.progress),
-                    const SizedBox(width: 8),
-                    Icon(Icons.pause),
-                  ],
-                ),
-                onPressed: recorder.pauseRecording,
-              ),
-              const SizedBox(width: 8),
-              VolumeDisplay(recorder.progress, false),
-            ],
-          );
+          return _MiniRecordingControls(recorder);
         } else {
           return CircularIconButton(
             iconData: Icons.fiber_manual_record_rounded,
@@ -52,6 +23,58 @@ class MiniRecorderDisplay extends StatelessWidget {
           );
         }
       },
+    );
+  }
+}
+
+class _MiniPausedControls extends StatelessWidget {
+  // Displayed when paused, showing resume and stop buttons
+  final Recorder _recorder;
+
+  _MiniPausedControls(this._recorder);
+
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircularIconButton(
+          iconData: Icons.stop,
+          onPressed: _recorder.stopRecording,
+        ),
+        CircularIconButton(
+          iconData: Icons.play_arrow,
+          onPressed: _recorder.resumeRecording,
+        ),
+      ],
+    );
+  }
+}
+
+class _MiniRecordingControls extends StatelessWidget {
+  // Displayed when recording, showing record time, volume, and a pause button
+  final Recorder _recorder;
+
+  _MiniRecordingControls(this._recorder);
+
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        VolumeDisplay(_recorder.progress, true),
+        const SizedBox(width: 8),
+        RoundedButton(
+          child: Row(
+            children: [
+              DurationDisplay(_recorder.progress),
+              const SizedBox(width: 8),
+              Icon(Icons.pause),
+            ],
+          ),
+          onPressed: _recorder.pauseRecording,
+        ),
+        const SizedBox(width: 8),
+        VolumeDisplay(_recorder.progress, false),
+      ],
     );
   }
 }
