18e648da4615526e60ff6c4a72feb460a10b4991
KangRuiYu <48875810+KangRuiYu@users.noreply.github.com>
2021-09-07 17:11:16 -0700

    Updated version of app to 1.0.0

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,7 +1,7 @@
 /// Meant to be used with the import alias 'app_constants'.
 
 const String name = 'Voice Scribe';
-const String version = '0.6.0';
+const String version = '1.0.0';
 const String copyright = 'Copyright (c) 2021 Kang Rui Yu';
 const String description =
     'Audio recording and playback app that transcribes speech in realtime.';
